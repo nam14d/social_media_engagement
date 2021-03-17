e2a5b0534f37bda9266108502cb04f03a52a8fe6
Nick Marangi <70975364+nam14d@users.noreply.github.com>
2021-03-17 14:52:12 -0700

    Add files via upload

--- social_media_analysis.sql
+++ social_media_analysis.sql
@@ -12,6 +12,7 @@ CREATE TABLE user_profile (
 CREATE TABLE post(
     post_id INT IDENTITY(30000,1) PRIMARY KEY,
     profile_id INT NOT NULL,
+    photo_id INT NOT NULL,
     post_date DATETIME NOT NULL,
     tweet_text VARCHAR(1000) NOT NULL,
     like_count INT NOT NULL,
@@ -37,25 +38,18 @@ CREATE TABLE post_hashtag(
 --4. Photo -- 
 CREATE TABLE photo(
     photo_id INT IDENTITY(64000,1) PRIMARY KEY,
-    photo_url VARCHAR(1000)
+    photo_url VARCHAR(1000) NOT NULL,
+    vader_score FLOAT(2) NOT NULL,
+    vader_sentiment VARCHAR(8) NOT NULL
 );
 
--- 5. Photo_Post -- 
-CREATE TABLE photo_post(
-    photo_id INT NOT NULL,
-    post_id INT NOT NULL,
-    PRIMARY KEY (photo_id, post_id),
-    CONSTRAINT photo_id FOREIGN KEY (photo_id) REFERENCES photo(photo_id),
-    CONSTRAINT post_id FOREIGN KEY (post_id) REFERENCES post(post_id)
-);
-
--- 6. Sentiment --
+-- 5. Sentiment --
 CREATE TABLE sentiment(
     sentiment_id INT IDENTITY(74000,1) PRIMARY KEY,
-    sentiment_name VARCHAR(100)
+    sentiment_name VARCHAR(100) NOT NULL
 );
 
---7. Photo_Sentiment -- 
+--6. Photo_Sentiment -- 
 CREATE TABLE photo_sentiment(
     photo_id INT NOT NULL,
     sentiment_id INT NOT NULL,
@@ -64,59 +58,65 @@ CREATE TABLE photo_sentiment(
     CONSTRAINT sentiment_id FOREIGN KEY (sentiment_id) REFERENCES sentiment(sentiment_id)
 );
 
--- 8. Trends --
+-- 7. Trends --
 CREATE TABLE trends(
     trend_id INT IDENTITY(4300,1),
     trend_name VARCHAR(40)
-)
+);
 -----------------------------------------------------------------------------------------------------
 -- Temporary Helper Tables --
 
 -- User_Profile Helper --
 CREATE TABLE temp(
     temp_id INT IDENTITY(100, 1) PRIMARY KEY,
-    profile_name VARCHAR(100) NOT NULL,
+    profile_name VARCHAR(1000) NOT NULL,
+    photo_url VARCHAR(1000) NOT NULL,
     post_date DATETIME NOT NULL,
     tweet_text VARCHAR(1000) NOT NULL,
     like_count INT NOT NULL,
     retweet_count INT NOT NULL
-)
+);
 
--- Post_Profile Helper --
+-- Post_Hashtag Helper --
 CREATE TABLE temp(
     temp_id INT IDENTITY(100,1) PRIMARY KEY,
     tweet VARCHAR(1000) NOT NULL,
     hashtag VARCHAR(200) NOT NULL
-)
+);
 
--- Photo_Post Helper --
 CREATE TABLE temp(
     temp_id INT IDENTITY(100,1) PRIMARY KEY,
-    tweet VARCHAR(1000) NOT NULL,
-    photo_url VARCHAR(1000) NOT NULL
-)
+    photo_url VARCHAR(1000) NOT NULL,
+    label VARCHAR(200) NOT NULL
+);
 ----------------------------------------------------------------------------------------------------
 -- Filling an Existing Table with values Brought Together by a Helper Table --
 
 -- Filling post table with appropriate values
-INSERT INTO dbo.post(profile_id, post_date, tweet_text, like_count, retweet_count)
+INSERT INTO dbo.post(profile_id, photo_id, post_date, tweet_text, like_count, retweet_count)
 SELECT 
-    p.profile_id, 
-    post_date, 
-    tweet_text, 
-    like_count, 
-    retweet_count 
+    p.profile_id,
+    ph.photo_id,
+    post_date,
+    tweet_text,
+    like_count,
+    retweet_count
 FROM 
     dbo.temp t 
 JOIN 
     dbo.user_profile p
 ON 
     t.profile_name = p.profile_name
+JOIN 
+    dbo.photo ph
+ON
+    ph.photo_url = t.photo_url;
 
 -- Filling post_hashtag --
 INSERT INTO dbo.post_hashtag(post_id, hashtag_id)
 SELECT DISTINCT
-    post_id,hashtag_id 
+    post_id,
+    hashtag_id 
 FROM 
     dbo.hashtag h 
 JOIN 
@@ -126,19 +126,114 @@ ON
 JOIN 
     dbo.post p 
 ON 
-    p.tweet_text = t.tweet
+    p.tweet_text = t.tweet;
 
--- Filling Photo_Post --
-INSERT INTO dbo.photo_post(photo_id, post_id)
-SELECT 
-    photo_id,post_id
+-- Photo - Sentiment -- 
+INSERT INTO dbo.photo_sentiment(photo_id, sentiment_id)
+SELECT DISTINCT
+    p.photo_id,
+    s.sentiment_id
 FROM 
-    dbo.photo p 
+    dbo.photo p
 JOIN 
-    dbo.temp t 
+    dbo.temp t
 ON 
-    p.photo_url = t.photo_url 
+    p.photo_url = t.photo_url
 JOIN 
-    dbo.post post 
+    dbo.senitment s
 ON 
-    post.tweet_text = t.tweet
+    s.sentiment_name = t.label
+
+
+-- Indices and Procedures/Functions --
+
+ -- Nonclustered Index on profile_name
+-- CREATE NONCLUSTERED INDEX IX_profile_name
+-- ON dbo.user_profile(profile_name)
+-- GO
+
+ -- Nonclustered Index on hashtag_text
+-- CREATE NONCLUSTERED INDEX IX_hashtag_text
+-- ON dbo.hashtag(hashtag_text)
+-- GO
+
+ -- Procedures -- 
+
+ -- Adding new values -- 
+
+-- Adding a new post --
+IF OBJECT_ID('dbo.insertPost','P') IS NOT NULL
+    DROP PROCEDURE dbo.insertPost;
+GO
+--Create Procedure
+-- Create insertPost -- 
+CREATE PROCEDURE dbo.insertPost
+    @profile_id INT, 
+    @photo_id INT, 
+    @post_date DATETIME, 
+    @tweet_text VARCHAR(1000), 
+    @like_count INT, 
+    @retweet_count INT
+AS
+    BEGIN TRAN T1
+        INSERT INTO [dbo].[post](profile_id, photo_id, post_date, tweet_text, like_count, retweet_count)
+        VALUES(@profile_id, @photo_id, @post_date, @tweet_text, @like_count, @retweet_count)
+    IF @@ERROR <> 0
+        ROLLBACK TRAN T1
+    ELSE
+        COMMIT TRAN T1
+GO
+-- Create insertUser -- 
+CREATE PROCEDURE dbo.insertUser
+    @profile_name VARCHAR(1000), 
+    @follower_count INT
+AS
+    BEGIN TRAN T1
+        INSERT INTO [dbo].[user_profile](profile_name, follower_count)
+        VALUES(@profile_name, @follower_count)
+    IF @@ERROR <> 0
+        ROLLBACK TRAN T1
+    ELSE
+        COMMIT TRAN T1
+GO
+
+-- Create insertUser -- 
+CREATE PROCEDURE dbo.insertUser
+    @profile_name VARCHAR(1000), 
+    @follower_count INT
+AS
+    BEGIN TRAN T1
+        INSERT INTO [dbo].[user_profile](profile_name, follower_count)
+        VALUES(@profile_name, @follower_count)
+    IF @@ERROR <> 0
+        ROLLBACK TRAN T1
+    ELSE
+        COMMIT TRAN T1
+GO
+
+-- Create insertHashtag -- 
+CREATE PROCEDURE dbo.insertHashtag
+    @hashtag_text VARCHAR(200)
+AS
+    BEGIN TRAN T1
+        INSERT INTO [dbo].[hashtag](hashtag_text)
+        VALUES(@hashtag_text)
+    IF @@ERROR <> 0
+        ROLLBACK TRAN T1
+    ELSE
+        COMMIT TRAN T1
+GO
+-- Create insertPhoto --
+CREATE PROCEDURE dbo.insertPhoto
+    @photo_url VARCHAR(1000),
+    @vader_score FLOAT(2),
+    @vader_sentiment VARCHAR(8)
+AS
+    BEGIN TRAN T1
+        INSERT INTO [dbo].[photo](photo_url, vader_score, vader_sentiment)
+        VALUES(@photo_url, @vader_score, @vader_sentiment)
+    IF @@ERROR <> 0
+        ROLLBACK TRAN T1
+    ELSE
+        COMMIT TRAN T1
+GO
